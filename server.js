cb03e39ad74e8f04d7ed319973f291005b15dfd2
Tyler Wheeler <tyler0wheeler@gmail.com>
2021-02-05 11:30:08 -0500

    bug bug bug

--- server.js
+++ server.js
@@ -7,17 +7,6 @@ const bcrypt = require('bcrypt')
 const app = express()
 const multer = require('multer')
 
-// //Set storage engine
-// const storage = multer.diskStore({
-//   destination: '/luthiers',
-//   filename: function(req, file, cb){
-//     cb(null,file.fieldname + '-' + Date.now() + path.extname(file.originalname))
-//   }
-// })
-// //Init Upload
-// const upload = multer({
-//   storage: storage
-// }).single('audioSample1')
 
 
 // env variables
